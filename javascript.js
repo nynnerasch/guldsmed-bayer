ce47dab96ae02110feab90a77959c080dce4e6cc
nynnerasch <nynne.andersen@yahoo.dk>
2023-05-23 22:48:19 +0200

    lidt java

--- javascript.js
+++ javascript.js
@@ -1,3 +1,10 @@
+window.addEventListener("load", setup);
+const endpoint = "http://guldsmedbayer.josefinemoerchh.com/wp-json/wp/v2/";
+
+function setup() {
+  getCategories();
+}
+
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
@@ -17,4 +24,21 @@ navLink.forEach((n) => n.addEventListener("click", closeMenu));
 function closeMenu() {
   hamburger.classList.remove("active");
   navMenu.classList.remove("active");
+}
+
+/* SHOP */
+function getCategories() {
+  fetch(endpoint + "categories?parent=2")
+    .then((res) => res.json())
+    .then(setupCategories);
+}
+
+function setupCategories(catArray) {
+  const template = document.querySelector("templatecategorytemplate").Content;
+  const parentElement = document.querySelector("main");
+  catArray.forEach((cat) => {
+    const copy = template.cloneNode(true);
+    copy.querySelector("h2").textContent = cat.name;
+    parentElement.appendChild(copy);
+  });
 }
