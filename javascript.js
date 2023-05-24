9fafb7a36d7315208be6ffa1482c5fed928717f4
nynnerasch <nynne.andersen@yahoo.dk>
2023-05-24 15:21:07 +0200

    javascript som ikke fucking virker ordentligt

--- javascript.js
+++ javascript.js
@@ -29,17 +29,40 @@ function closeMenu() {
 /* SHOP */
 
 function getCategories() {
-  fetch(endpoint + "categories?parent=2")
+  fetch(endpoint + "categories?parent=2&_fields.name")
     .then((res) => res.json())
     .then(setupCategories);
 }
+function getTheSmykker() {
+  fetch(
+    "http://guldsmedbayer.josefinemoerchh.com/wp-json/wp/v2/smykke?per_page=100&_embed"
+  )
+    .then((res) => res.json())
+    .then(setupSmykker);
+}
+
+function setupSmykker(smykkeArray) {
+  console.log(smykkeArray);
+  const template = document.querySelector("template#smykke-template").content;
+  const parentElement = document.querySelector(".shop-page main section");
+  smykkeArray.forEach((smykke) => {
+    const copy = template.cloneNode(true);
+    copy.querySelector("img").src =
+      smykke._embedded[
+        "wp:featuredmedia"
+      ][0].media_details.sizes.full.source_url;
+    copy.querySelector("h3").textContent = `${smykke.title.rendered}`;
+    parentElement.appendChild(copy);
+  });
+}
 
 function setupCategories(catArray) {
-  const template = document.querySelector("templatecategorytemplate").Content;
+  const template = document.querySelector("template#category-template").content;
   const parentElement = document.querySelector("main");
   catArray.forEach((cat) => {
     const copy = template.cloneNode(true);
-    copy.querySelector("h3").textContent = cat.name;
+    copy.querySelector("h2").textContent = cat.name;
     parentElement.appendChild(copy);
   });
+  getTheSmykker();
 }
